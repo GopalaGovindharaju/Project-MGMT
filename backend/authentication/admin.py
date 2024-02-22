d590c2718bb01ec5c6883e0dc7010773d9b5a608
Gops 'eh <gopala200218@gmail.com>
2024-02-22 15:43:02 +0530

    signuptabl

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SignUp_Table
 
-# Register your models here.
+class SignUp_TableAdmin(admin.ModelAdmin):
+    list_display = ('ID', 'Name', 'Role', 'Department')
+    
+admin.site.register(SignUp_Table,SignUp_TableAdmin)
