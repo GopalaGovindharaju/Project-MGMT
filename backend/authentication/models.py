0b5170b676285c65819a9548a1854b66cf222709
Gops 'eh <gopala200218@gmail.com>
2024-02-22 15:32:10 +0530

    signuptable

---
+++ backend/authentication/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+
+class SignUp_Table(models.Model):
+    ID = models.CharField(primary_key=True, max_length = 20, unique = True)
+    Name = models.CharField(max_length = 50, default = None)
+    Password = models.CharField(max_length = 50)
+    Role = models.CharField(max_length = 30)
+    Department = models.CharField(max_length = 50, unique = True)
+
+    def __str__(self):
+        return f"{self.ID} {self.Name} {self.Department}"
+    
+    def set_password(self, raw_password):
+        self.Password = make_password(raw_password)
+    
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.Password)
+    
+
+
---
+++ backend/authentication/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import SignUp_Table
+from .serializers import apiserializer
+
+
+
+
