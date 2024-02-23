2027c2c4dab1ebc55fea9d040885701f43411402
Gops 'eh <gopala200218@gmail.com>
2024-02-23 16:36:26 +0530

    fr

--- backend/student/admin.py
+++ backend/student/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Review_0
+from .models import Review_2
 
 class Review_0_Admin(admin.ModelAdmin):
     list_display = ('ID', 'Title', 'Abstract', 'Base_Paper', 'PPT', 'Title_Status', 'Abstract_Status', 'Base_Paper_Status', 'PPt_Status', 'Guide_Status', 'Hod_Status', )
 
-class Review_2_Admin(admin.ModuleAdmin):
+class Review_2_Admin(admin.ModelAdmin):
     list_display = ('ID', 'Implementation_80P', 'Report_RoughCopy', 'PPT', 'Project_Publish', 'Implement_Status', 'Report_Status', 'PPt_Status', 'Guide_Status', 'Hod_Status',) 
 
 admin.site.register(Review_0, Review_0_Admin)
+admin.site.register(Review_2, Review_2_Admin)
