1c9d3155f88af788794f97d2d30795724dd162f3
gopi <gopivelraj7169@gmail.com>
2024-02-23 16:34:44 +0530

    pull

--- backend/student/admin.py
+++ backend/student/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Review_0
+from .models import Review_1
 
 class Review_0_Admin(admin.ModelAdmin):
     list_display = ('ID', 'Title', 'Abstract', 'Base_Paper', 'PPT', 'Title_Status', 'Abstract_Status', 'Base_Paper_Status', 'PPt_Status', 'Guide_Status', 'Hod_Status', )
+
+class Review_1_Admin(admin.ModelAdmin):
+    list_display = ('ID', 'Architecture', 'Modules', 'Modules_Description', 'Litrature_survey', 'Outcome_Images', 'PPT', 'Architecture', 'Modules', 'Modules_Description', 'Litrature_survey', 'Outcome_Images', 'PPt_Status', 'Guide_Status', 'Hod_Status', )
+    
     
 admin.site.register(Review_0, Review_0_Admin)
+admin.site.register(Review_1,Review_1_Admin)
--- backend/student/serializers.py
+++ backend/student/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Review_0
+from .models import Review_1
 
 class Review_0_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Review_0
         fields = ('ID', 'Title', 'Abstract', 'Base_Paper', 'PPT', 'Title_Status', 'Abstract_Status', 'Base_Paper_Status', 'PPt_Status', 'Guide_Status', 'Hod_Status', )
+
+class Review_1_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review_1
+        feilds = ('ID', 'Architecture', 'Modules', 'Modules_Description', 'Litrature_survey', 'Outcome_Images', 'PPT', 'Architecture', 'Modules', 'Modules_Description', 'Litrature_survey', 'Outcome_Images', 'PPt_Status', 'Guide_Status', 'Hod_Status', )
