13a0e99cd97a2f9e8ed3090a71b5750124e225b6
chitra lakshmi <chitraraveendran2002@gmail.com>
2024-02-23 16:28:21 +0530

    back

--- backend/student/serializers.py
+++ backend/student/serializers.py
@@ -5,3 +5,6 @@ class Review_0_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Review_0
         fields = ('ID', 'Title', 'Abstract', 'Base_Paper', 'PPT', 'Title_Status', 'Abstract_Status', 'Base_Paper_Status', 'PPt_Status', 'Guide_Status', 'Hod_Status', )
+
+        model = Review_2
+        fields = ('ID', 'Implementation_80P', 'Report_RoughCopy', 'PPT', 'Project_Publish', 'Implement_Status', 'Report_Status', 'PPt_Status', 'Guide_Status', 'Hod_Status',)
