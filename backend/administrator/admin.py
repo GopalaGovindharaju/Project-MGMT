77ff45c7b99e4105b7184a68f36b16ae78f0f291
gopi <gopivelraj7169@gmail.com>
2024-02-23 11:45:32 +0530

    'pull'

--- backend/administrator/admin.py
+++ backend/administrator/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Guide_Info
 
 class Guide_InfoAdmin(admin.ModelAdmin):
-    list_display = ('ID', 'Name', 'Designation', 'Department', )
+    list_display = ('ID', 'Name', 'Designation', 'Department', 'PanelMember')
     
 admin.site.register(Guide_Info, Guide_InfoAdmin)
