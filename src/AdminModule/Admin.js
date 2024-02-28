ba297937efeba16c3a97b2ff1c7e0ddeb89697be
gopi <gopivelraj7169@gmail.com>
2024-02-28 16:09:19 +0530

    'pull'

--- src/AdminModule/Admin.js
+++ src/AdminModule/Admin.js
@@ -257,38 +257,58 @@ function Admin() {
                     <div className="project-box-content-header">
                       <p className="box-content-header">{project.Title}</p>
                     </div>
+                    <h6  style={{marginBottom:'-10px'}}>Reviews Completion</h6>
+
+                    <div style={{display:'flex', flexDirection:'row', padding :'10px',justifyContent:'space-between'}}>
                     <div className="box-progress-wrapper">
-                      <p className="box-progress-header">Progress</p>
+                      <p className="box-progress-header"style={{width:'65px'}}></p>
                       <div className="box-progress-bar">
                         <span
                           className="box-progress"
-                          style={{
-                            width: `${Math.round(project.Review)}%`,
-                            backgroundColor: project.textColor,
-                          }}
+                          style={{ width: `${Math.round(project.Review)}%`, backgroundColor: project.textColor }}
                         ></span>
                       </div>
-                      <p className="box-progress-percentage">
-                        {Math.round(project.Review)}%
-                      </p>
+                      <div className="box-progress-footer"> <p>0</p>  </div>
                     </div>
+
                     <div className="box-progress-wrapper">
-                      <p className="box-progress-header">Progress</p>
+                    <p className="box-progress-header"style={{width:'65px'}}></p>
                       <div className="box-progress-bar">
                         <span
                           className="box-progress"
-                          style={{
-                            width: `${Math.round(project.Review)}%`,
-                            backgroundColor: project.textColor,
-                          }}
+                          style={{ width: `${Math.round(project.Review)}%`, backgroundColor: project.textColor }}
                         ></span>
                       </div>
-                      <p className="box-progress-percentage">
-                        {Math.round(project.Review)}%
-                      </p>
+                      <div className="box-progress-footer"> <p>1</p>  </div>
+
                     </div>
+
+                    <div className="box-progress-wrapper">
+                    <p className="box-progress-header" style={{width:'65px'}}></p>
+                      <div className="box-progress-bar">
+                        <span
+                          className="box-progress"
+                          style={{ width: `${Math.round(project.Review)}%`, backgroundColor: project.textColor }}
+                        ></span>
+                      </div>
+                      <div className="box-progress-footer"> <p>2</p>  </div>
+
+                    </div>
+
+                    <div className="box-progress-wrapper">
+                    <p className="box-progress-header"style={{width:'65px'}}></p>
+                      <div className="box-progress-bar">
+                        <span
+                          className="box-progress"
+                          style={{ width: `${Math.round(project.Review)}%`, backgroundColor: project.textColor }}
+                        ></span>
+                      </div>
+                      <div className="box-progress-footer"> <p>3</p>  </div>
+                    </div>     
+                    </div>
+
                     
-                    <div className="project-box-footer">
+                    <div className="project-box-footer"> 
                       <div className="participants">
                         <p className="box-content-subheader">
                           {project.Project_Guide}
