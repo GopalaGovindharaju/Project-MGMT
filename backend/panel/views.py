5e986a2e9cf0f33dce68bbdb3ee47ad0a340dafe
gopi <gopivelraj7169@gmail.com>
2024-03-01 11:22:43 +0530

    'pull'

--- backend/panel/views.py
+++ backend/panel/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-
-def panel_review0(request):
-    print('')
+from serializers import serializers
+@api_view(['POST'])
+def get_guide_details(request):
+    if request.method == 'POST':
+        data = request.data
+        Guide_ID=data.get('id')
+        
+        if Student_Info.objects.filter(Guide_ID=Guide_ID).exists():
+            subquery = Student_Info.objects.filter(Guide_ID=Id, Batch=OuterRef('Batch')).values('ID')
+            teams = Student_Info.objects.filter(ID__in=Subquery(subquery))
+            serializer = Student_InfoSerializer(teams, many=True)
